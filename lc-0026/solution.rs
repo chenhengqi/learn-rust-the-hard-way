9bf1ab9182d2399a2eb1434ae826883b60e98940
chenhengqi <chenhengqi@outlook.com>
2020-05-07 08:13:16 +0800

    add lc-0026 solutions

---
+++ lc-0026/solution.rs
@@ -0,0 +1,19 @@
+impl Solution {
+    pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+        if nums.is_empty() {
+            return 0;
+        }
+        let n = nums.len();
+        let mut i = 0;
+        for j in 1..n {
+            if nums[j] != nums[i] {
+                nums[i + 1] = nums[j];
+                i += 1;
+            }
+        }
+        (i + 1) as i32
+
+        // nums.dedup();
+        // nums.len() as i32
+    }
+}
